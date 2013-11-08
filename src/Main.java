b4a1f0d60f5e6381197d8362b0b8a9c559e8f893
larshp <larshp@hotmail.com>
2013-11-08 15:21:19 +0100

    more

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,16 @@
 
 public class Main {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public Main() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	public static void main(String[] args) {
+
+		String vocabulary = "0123456789ABCDEF";
+		int length = 16;
+		
 	}
 
 }
