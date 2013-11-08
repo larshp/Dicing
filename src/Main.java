7e7ee096defcdf2065531f35689d9eb23989ba82
larshp <larshp@hotmail.com>
2013-11-08 15:26:24 +0100

    foo

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,6 @@
 
 public class Main {
 
-	public Main() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-
 	public static void main(String[] args) {
 
 		String vocabulary = "0123456789ABCDEF";
---
+++ src/Node.java
@@ -0,0 +1,10 @@
+
+public interface Node {
+
+	public boolean find(String str);
+	
+	public void insert(String str);
+	
+	public void delete(String str);
+	
+}
