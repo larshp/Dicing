961a2413a3edde91f63a674210b6f6502c306ee5
larshp <larshp@hotmail.com>
2013-11-28 13:30:42 +0100

    more stuff

--- src/Controller.java
+++ src/Controller.java
@@ -2,37 +2,63 @@ public class Controller {
 
 	private static byte nodedata[];
 
+	private static byte[] foo1;
+	private static byte[] foo2;
+	private static byte[] foo3;
+	private static byte[] foo4;
+	
+	private static final int node_levels = 5;
+	
+//   50 =   800 mb	
+//   60 =   960 mb
+// 2000 = 32000 mb
+	private static final int leaf_length = 60;
+	
+	
 	public static void init() {
 
 		int total = 0;
-		int bytes;
-		
-		System.out.println("Vocabulary length: " + Main.vocabulary.length());
-		bytes = (int)Math.ceil(Main.vocabulary.length() / 8.0);
-		System.out.println("Node size: " + bytes + " bytes");
-		
-		// calculate levels
-		for (int i = 0; i <= 7; i++) {
-			int local = (int) Math.pow(Main.vocabulary.length(), i);
-			System.out.println("Level: "+i + ", nodes: " + local);
-			total = total + local;
-		}
-		System.out.println("Nodes: " + total);
+		int last = 0;
+		long leafnodes = 0;
+		int bytes = 0;
 
-		nodedata = new byte[total*bytes];
+		System.out.println("Vocabulary length: " + Main.vocabulary.length());
+		System.out.println("Depth: " + Main.depth);
+
+		bytes = (int) Math.ceil(Main.vocabulary.length() / 8.0);
+		System.out.println("Node size: " + bytes + " bytes");
+
+		// calculate levels
+		for (int i = 0; i <= node_levels; i++) {
+			last = (int) Math.pow(Main.vocabulary.length(), i);
+			System.out.println("Level: " + i + ", nodes: " + last);
+			total = total + last;
+		}
+		leafnodes = last * Main.vocabulary.length();
+
+		System.out.println("Nodes:\t" + total + "("
+				+ (total * bytes / 1024 / 1024) + "mb)");
+		System.out.println("Leaves:\t" + leafnodes + "("
+				+ (leafnodes * leaf_length / 1024 / 1024) + "mb)");
+		System.out.println("Bits per leaf:\t" + leaf_length * 8);
+		System.out.println("Chars per leaf:\t" + leaf_length * 8 / 4); // todo 4 = 16 voc length
+
+		nodedata = new byte[total * bytes];
+//		leafdata = new byte[leafnodes * leaf_length];
+
+		foo1 = new byte[16777216*40];		
+		foo2 = new byte[16777216*10];
+		foo3 = new byte[16777216*10];
 		
 		System.out.println("Allocated");
+
 	}
 
-	public static boolean find(String str) {
-		
-		
-		return false;
-	}
-
+	// returns
+	// true if inserted successfully
+	// false if already exists
 	public static boolean insert(String str) {
-		
-		
+
 		return false;
 	}
 
--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,28 @@
-
 public class Main {
 
-//	public static String vocabulary = "0123456789ABCDEFGHKMNPRSTVWXYZ";
+	// public static String vocabulary = "0123456789ABCDEFGHKMNPRSTVWXYZ";
 	public static String vocabulary = "0123456789ABCDEF";
-	
+	public static int depth = 11;
+
 	public static void main(String[] args) {
 
 		Controller.init();
-		Controller.insert("ABC2384756192784");
-		
-//		memInfo();
+	//	Controller.insert("0000000000000000");
+
+
+		memInfo();
 	}
 
-	
-    public static void memInfo() {                   
-    	int mb = 1024*1024;                  
-    	//Getting the runtime reference from system        
-    	Runtime runtime = Runtime.getRuntime();                
-    	System.out.println("##### Heap utilization statistics [MB] #####");     
-    	//Print used memory        
-    	System.out.println("Used Memory:"         
-    	+ (runtime.totalMemory() - runtime.freeMemory()) / mb);      
-    	//Print free memory      
-    	System.out.println("Free Memory:"     
-    	+ runtime.freeMemory() / mb);        
-    	//Print total available memory      
-    	System.out.println("Total Memory:" + runtime.totalMemory() / mb);          
-    	//Print Maximum available memory        
-    	System.out.println("Max Memory:" + runtime.maxMemory() / mb);     
-    }
-	
+	public static void memInfo() {
+		int mb = 1024 * 1024;
+
+		Runtime runtime = Runtime.getRuntime();
+		System.out.println("##### Heap utilization statistics [MB] #####");
+		System.out.println("Used Memory: "
+				+ (runtime.totalMemory() - runtime.freeMemory()) / mb);
+		System.out.println("Free Memory: " + runtime.freeMemory() / mb);
+		System.out.println("Total Memory: " + runtime.totalMemory() / mb);
+		System.out.println("Max Memory: " + runtime.maxMemory() / mb);
+	}
+
 }
